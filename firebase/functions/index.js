72046406461e5968f9b0163f90c657ddc44b51c3
nErumin <nerumin90@gmail.com>
2017-06-25 13:56:11 +0900

    Delete unused cloud function.

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -11,36 +11,6 @@ expressApp.use(bodyParser.json())
 expressApp.use(bodyParser.urlencoded({ extended: true }))
 admin.initializeApp(functions.config().firebase)
 
-exports.Authenticate = functions.https.onRequest(function(appRequest, appResponse) {
-    var apiUri = 'http://zerobot.herokuapp.com/hubot/otp/' + appRequest.body.username + '/'
-
-    var options = {
-        method: 'POST',
-        uri: apiUri,
-        body: { 
-            token: appRequest.body.token
-        },
-        json: true,
-        resolveWithFullResponse: true,
-        simple: true
-    };
-
-    console.log('Request to "' + apiUri 
-        + '" with token = "' + appRequest.body.token + '", user = "' + appRequest.body.username +'"');
-
-    requestPromise(options)
-        .then(function (apiResponse) {
-            console.log('Response code = ' + apiResponse.statusCode);
-            console.log('The API authentication process was successful? = ' + (apiResponse.statusCode == 200));
-            appResponse.status(apiResponse.statusCode).end();
-        })
-        .catch(function (error) {
-            console.log('Error happened in the API request.');
-            console.log(error);
-            appResponse.status(400).end();
-        })
-})
-
 exports.SetMember = functions.https.onRequest(function (request, response) {
     var dataUri = 'member/' + request.body.username;
     var memberData = {
